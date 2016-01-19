c347824a1eb23668ed4d2c3976191c4aba9aeb7f
Michael Lollis <lollis1984@gmail.com>
2016-01-19 14:38:57 -0600

    Initial Commit

---
+++ Pokedex-by-mikes-concepts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex-by-mikes-concepts
+//
+//  Created by Michael Lollis on 1/19/16.
+//  Copyright © 2016 mikesconcepts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
